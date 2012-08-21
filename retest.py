3d4a2da4aed6b470e7d0a9449311c08bc8acd0f5
Vegard Angell <v.angell123@gmail.com>
2012-08-22 01:52:15 +0200

    cleanup

--- retest.py
+++
@@ -1,8 +0,0 @@
-import re
-
-fname='/home/cyb/Documents/cyb_medlem_v2/medlemslister/medlemmer_v12.json'
-
-m=re.search('.+medlemmer_(\S)(\d\d)\.json', fname)
-print m.group(0)
-print m.group(1)
-print m.group(2)
